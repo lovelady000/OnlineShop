fe79e18824af68df56aa3cb3e75ccb516621e37f
nndun <nndun@ADMIN>
2016-07-27 20:21:42 +0700

    1

--- OnlineShop.Web/Api/ProductCategoryController.cs
+++ OnlineShop.Web/Api/ProductCategoryController.cs
@@ -3,7 +3,9 @@ using OnlineShop.Model.Models;
 using OnlineShop.Service;
 using OnlineShop.Web.Infrastructure.Core;
 using OnlineShop.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -22,13 +24,23 @@ namespace OnlineShop.Web.Api
         }
 
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int total = 0;
                 var model = _productCategoryService.GetAll();
-                var responeData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var respone = request.CreateResponse(HttpStatusCode.OK, responeData);
+                total = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+                var responeData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responeData,
+                    Page = page,
+                    TotalCount = total,
+                    TotalPages = (int)Math.Ceiling((decimal)total / pageSize),
+                };
+                var respone = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return respone;
             });
         }
--- OnlineShop.Web/Infrastructure/Core/PaginationSet.cs
+++ OnlineShop.Web/Infrastructure/Core/PaginationSet.cs
@@ -6,7 +6,7 @@ namespace OnlineShop.Web.Infrastructure.Core
     public class PaginationSet<T>
     {
         public int Page { set; get; }
-        public int Count { get { return Items != null ? Items.Count() : 0} }
+        public int Count { get { return Items != null ? Items.Count() : 0; } }
         public int TotalPages { set; get; }
         public int TotalCount { set; get; }
         public IEnumerable<T> Items { set; get; }
