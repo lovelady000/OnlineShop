a688c33f3a7faa7433c24b7eb36100c09edfab7d
Dũng Nguyễn <Dng Nguyn>
2016-07-31 20:54:41 +0700

    31-07

--- OnlineShop.Web/app/components/product_categories/productCategoryListController.js
+++ OnlineShop.Web/app/components/product_categories/productCategoryListController.js
@@ -2,8 +2,8 @@
 (function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
-    function productCategoryListController($scope, apiService) {
+    productCategoryListController.$inject = ['$scope', 'apiService','notificationService'];
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
@@ -23,6 +23,9 @@
                 }
             };
             apiService.get('/api/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('Không có bản ghi nào được tìm thấy!');
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
